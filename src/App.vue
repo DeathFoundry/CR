483b544c494cd822a5dca81a3b16292b53bf86d8
DeathFoundry <joshwalter85@gmail.com>
2024-05-01 16:50:30 -0400

    Moar stuff

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Auth1 from './components/Auth1.vue';
 import DarkMode from './components/DarkMode.vue';
+import TeamMembers from './components/TeamMembers.vue';
 </script>
 
 <template>
-<Auth1><</Auth1>
 <DarkMode></DarkMode>
+<Auth1><</Auth1>
+<TeamMembers></TeamMembers>
 </template>
+
 
