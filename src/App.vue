801688af6a52aebbfadcaabb974e1101e32f7b69
DeathFoundry <joshwalter85@gmail.com>
2024-05-01 18:21:08 -0400

    Good

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,12 @@
 import Auth1 from './components/Auth1.vue';
 import DarkMode from './components/DarkMode.vue';
 import TeamMembers from './components/TeamMembers.vue';
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from '@/components/ui/resizable'
 </script>
 
 <template>
     <DarkMode></DarkMode>
-
-    <ResizablePanelGroup direction="horizontal">
-    <ResizablePanel>
-
-        <Auth1><</Auth1>
-
-    </ResizablePanel>
-
-    <ResizableHandle with-handle />
-
-    <ResizablePanel>
-
-        <TeamMembers></TeamMembers>
-
-    </ResizablePanel>
-  </ResizablePanelGroup>
+    <Auth1><</Auth1>
+    <TeamMembers></TeamMembers>
 </template>
 
 
