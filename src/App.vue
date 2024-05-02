a0ccff074aea2ad59b02eb80eb511854ca6a3f3c
DeathFoundry <joshwalter85@gmail.com>
2024-05-01 23:13:00 -0400

    Happy dance

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,18 @@
 import Auth1 from './components/Auth1.vue';
 import DarkMode from './components/DarkMode.vue';
 import TeamMembers from './components/TeamMembers.vue';
+import axios from "axios";
+
+axios.get('https://api.torn.com/user/?selections=&key=7QNZtLcs9zelxrmD')
+.then (response => console.log(response))
+.catch(error => console.log(error))
+
 </script>
 
 <template>
+    <div>
+        
+    </div>
     <DarkMode></DarkMode>
     <Auth1><</Auth1>
     <TeamMembers></TeamMembers>
